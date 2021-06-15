78b186b46ae9d2efd84bd11de30a8855a5939dcb
sang hyeok hong <sanghyeokhong@sangui-MacBookAir.local>
2021-06-15 16:01:14 +0900

    feat: add screens, add dependencies for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import {View} from 'react-native';
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {Provider} from 'react-redux';
+import HomeScreen from './screens/Home';
+import CreateScreen from './screens/Create';
+import DetailScreen from './screens/Detail';
 import store from './store';
+
+const Stack = createStackNavigator<RootStack>();
 
 const App = () => {
   return (
     <Provider store={store}>
-      <View />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Create" component={CreateScreen} />
+          <Stack.Screen name="Detail" component={DetailScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
---
+++ src/screens/Home.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {StackScreenProps} from '@react-navigation/stack';
+
+interface Props extends StackScreenProps<RootStack, 'Home'> {}
+
+function HomeScreen({navigation}: Props) {
+  const handlePressCreate = React.useCallback(() => {
+    navigation.navigate('Create');
+  }, [navigation]);
+
+  const handlePressListItem = React.useCallback(() => {
+    navigation.navigate('Detail');
+  }, [navigation]);
+
+  const {body, button} = styles;
+
+  return (
+    <View style={body}>
+      <Pressable style={button} onPress={handlePressCreate}>
+        <Text>Create</Text>
+      </Pressable>
+      <Pressable style={button} onPress={handlePressListItem}>
+        <Text>Item</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    borderWidth: 1,
+    borderColor: 'gray',
+  },
+});
+
+export default HomeScreen;
