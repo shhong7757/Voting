a0cc3630375b626516a765c533377815f781be7a
sang hyeok hong <sanghyeokhong@sangui-MacBookAir.local>
2021-06-24 18:11:15 +0900

    fix: My 화면에 세팅 버튼 svg 아이콘으로 변경

--- src/screens/Account.tsx
+++ src/screens/Account.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {SafeAreaView, View, Pressable, Text, StyleSheet} from 'react-native';
+import {SafeAreaView, View, Pressable, StyleSheet} from 'react-native';
 import {useSelector} from 'react-redux';
 import {RootState} from '../reducers';
 import AccountProfile from '../components/account/AccountProfile';
 import {useNavigation} from '@react-navigation/core';
+import Svg from '../components/common/Svg';
 
 function AccountScreen() {
   const account = useSelector((state: RootState) => state.auth.account);
@@ -23,7 +24,7 @@ function AccountScreen() {
           {account && <AccountProfile account={account} />}
         </View>
         <Pressable onPress={handlePressSetting}>
-          <Text>세팅</Text>
+          <Svg svg="Setting" />
         </Pressable>
       </View>
     </SafeAreaView>
